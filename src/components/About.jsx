bcdb6bfc0bfd8aab18c8bcc60be2c947838fd29c
Joachim Verleysen <jkverleysen@gmail.com>
2024-12-10 00:09:22 +0100

    Change about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,9 @@ function About() {
     <div id="About">
       <h1>About me</h1>
       <ul className="about-items">
-        <li>20 years old</li>
-        <li>Studying Computer Science</li>
-        <li>I play the piano</li>
-        <li>I like to travel</li>
+        <li>Computer nerd</li>
+        <li>Basketball</li>
+        <li>Piano</li>
       </ul>
       <div className="goto-btn">
         <a href="#Projects">Go to my coding projects</a>
